7cf8ad7039e3f2d1e9449585766e88e6509542f7
Rodrigo Javier Planas Herrera <developer.rodrigoherrera@gmail.com>
2021-02-11 14:19:28 -0600

    Se agrego validacion para campos en ingreso de la informacion

--- GitlabetcClient/Models/ClienteData.cs
+++ GitlabetcClient/Models/ClienteData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,22 @@ namespace GitlabetcClient.Models
 {
     public class ClienteData
     {
+        [Display(Name = "ID cliente")]
         public int idCliente { get; set; }
 
+        [Required(ErrorMessage = "Favor introduzca un nombre")]
+        [Display(Name = "Primer Nombre")]
         public string primerNombre { get; set; }
 
+        [Required(ErrorMessage = "Favor introduzca un apellido")]
+        [Display(Name = "Apellido")]
         public string primerApellido { get; set; }
 
+        [Required(ErrorMessage = "Introduzca una edad")]
+        [Display(Name = "Edad")]
         public int edad { get; set; }
 
+        [Display(Name = "Fecha creacion")]
         public DateTime fechaCreacion { get; set; }
     }
 }
